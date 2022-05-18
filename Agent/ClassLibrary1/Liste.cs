138785f73ba0b9953cd7c88435034b61e1f32871
aujault <Aurian.JAULT@etu.uca.fr>
2022-05-18 10:16:35 +0200

    ajout de c#

--- Agent/ClassLibrary1/Liste.cs
+++ Agent/ClassLibrary1/Liste.cs
@@ -6,26 +6,39 @@ using System.Threading.Tasks;
 
 namespace Classe
 {
-    public class Liste
+    public class ListeAg
     {
-        private List<Agent> duelist;
-        private List<Agent> controllers;
-        private List<Agent> initiators;
-        private List<Agent> sentinels;
+        private readonly List<Agent> duelist;
+        private readonly List<Agent> controllers;
+        private readonly List<Agent> initiators;
+        private readonly List<Agent> sentinels;
 
-        public Liste()
+        public ListeAg()
         {
-            List<Agent> duelist = new List<Agent>();
-            List<Agent> controllers = new List<Agent>();
-            List<Agent> initiators = new List<Agent>();
-            List<Agent> sentinels = new List<Agent>();
+            List<Agent> duelist = new List<Agent> { };
+            List<Agent> controllers = new List<Agent> { };
+            List<Agent> initiators = new List<Agent> { };
+            List<Agent> sentinels = new List<Agent> { };
         }
+
+        public void ForEach()
+        {
+            Console.WriteLine("Duelist: ");
+            duelist.ForEach(ag => Console.Write(ag));
+            Console.WriteLine("Controllers: ");
+            controllers.ForEach(ag => Console.Write(ag));
+            Console.WriteLine("Initiators: ");
+            initiators.ForEach(ag => Console.Write(ag));
+            Console.WriteLine("Sentinels: ");
+            sentinels.ForEach(ag => Console.Write(ag));
+        }
+
         public void Ajout(Agent a)
         {
-            if (a.Nom == "duelist") duelist.Add(a);
-            if (a.Nom == "controllers") controllers.Add(a);
-            if (a.Nom == "initiators") initiators.Add(a);
-            if (a.Nom == "sentinels") sentinels.Add(a);
+            if (a.Type == "Duelist") this.duelist.Add(a);
+            if (a.Type == "Controllers") this.controllers.Add(a);
+            if (a.Type == "Initiators") this.initiators.Add(a);
+            if (a.Type == "Sentinels") this.sentinels.Add(a);
         }
     }
 }
