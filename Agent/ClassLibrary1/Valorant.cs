c0770bc93e83894d72e7bb972e316038d9970738
aujault <Aurian.JAULT@etu.uca.fr>
2022-05-23 16:53:17 +0200

    c#

---
+++ Agent/ClassLibrary1/Valorant.cs
@@ -0,0 +1,23 @@
+﻿using Classe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    internal class Valorant : Agent
+    {
+        private string ultimate;
+        private string signature;
+        private string capA;
+        private string capC;
+
+        public Valorant(string nom, string type, ListeAg l, string nationalite)
+            : base(nom, type, l, nationalite)
+        {
+
+        }
+    }
+}
---
+++ Agent/ClassLibrary1/passif.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    internal class Passif
+    {
+        private string desc;
+        private string name;
+
+        public string Desc { get { return desc; } set { desc = value; } }
+        public string Name { get { return name; } set { name = value; } }
+        public Passif(string desc,string name)
+        {
+            Desc = desc;
+            Name = name;
+        }
+        public string Tostring()
+        {
+            return $"\n{Name}:\n{Desc}";
+        }
+    }
+    internal class Ultime: Passif
+    {
+        private int charges;
+        public int Charges { get { return charges; } set { charges = value; }}
+
+        public Ultime(int charges, string desc, string name) : base(desc, name)
+        {
+            Charges = charges;
+        }
+
+        public string toString()
+        {
+            return $"\n{Name}:\nCharges: {Charges}\n---\n{Desc}";
+        }
+    }
+}
